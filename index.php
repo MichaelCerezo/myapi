17ccc999c7d5f2662d8bb38a22d1731fa53a4175
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-05-08 11:17:31 -0700

    video 5 coded

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-
 //Configuration for our PHP Server
 set_time_limit(0);
 ini_set('default_socket_timeout', 300);
-
 session_start();
 
 //Make Constant using define.
@@ -11,6 +9,17 @@ define('clientID', 'd15f2eee69744a55b53457b348a2a9de');
 define('clientSecret', 'be8613e9ab8a45b8a68a028b4b693cd2');
 define('redirectURI', 'http://localhost/michaelapi/index.php');
 define('ImageDirectory', 'pics/');
+
+if isset(($_GET['code'])) {
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_settings = array('client_id' => clientID,
+		'client_secret' => clientSecret,
+		'grant_type' => 'authorization_code',
+		'redirect_uri' => redirectURI,
+		'code' => $code
+		);
+}
 
 ?>
 
@@ -20,7 +29,9 @@ define('ImageDirectory', 'pics/');
 	<title></title>
 </head>
 <body>
-	
+	<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
+	After getting approval we are now going to have the information so that we can play with it.
+	 -->
 	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
 	
 </body>
