d43fe2c736409a7799a83c992cafca58e5d5a29b
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-05-12 10:41:48 -0700

    video 7-8 coded

--- index.php
+++ index.php
@@ -20,8 +20,19 @@ if (isset($_GET['code'])) {
 		'code' => $code
 		);
 // cURL is what we use in PHP, it's a library calls to other API's
-}
+$cURL = curl_init($url); // setting a cURL session and we put in $url because that's where we are getting the data from.
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); // setting the POSTFIELDS to the array setup that we created.
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // setting it equal to 1 because we are getting strings back.
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // but in live work-production we want to set this to true.
 
+$result = curl_exec($curl);
+curl_close();
+
+$result = json_decode($reslt, true);
+echo $result['user']['username'];
+}
+else{
 ?>
 
 <!DOCTYPE html>
@@ -37,3 +48,6 @@ if (isset($_GET['code'])) {
 	
 </body>
 </html>
+<?php 
+}
+ ?>
